b9e45e9740d47d554e41b4be75835b7f0270ea0a
chuan5614515 <chinoputoamo20031031@gmail.com>
2023-02-06 20:24:58 +0100

    commit 4

--- Practica_git/src/git/practica_git.java
+++ Practica_git/src/git/practica_git.java
@@ -6,6 +6,6 @@ public class practica_git {
 	public static void main (String[] args) {
 		  Scanner sc = new Scanner (System.in);
 		  System.out.println("¿Has aprobado el primer trimestre?");
-		 
+
 	}
 }
